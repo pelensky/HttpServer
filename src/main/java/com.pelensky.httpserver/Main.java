0919c4c640ae5d5d55a256c4f3bd262c1d576ec6
Dan Pelensky <dan@pelensky.com>
2017-09-02 18:19:48 +0100

    Server accepts a connection

--- src/main/java/com.pelensky.httpserver/Main.java
+++ src/main/java/com.pelensky.httpserver/Main.java
@@ -3,6 +3,6 @@ package com.pelensky.httpserver;
 public class Main {
     public static void main(String[] args) {
         CommandLineArgumentParser parser = new CommandLineArgumentParser(args);
-        new HttpServer(parser.findPort(), parser.findDirectory());
+        new HttpServer(parser.findPort(), parser.findDirectory()).serve();
     }
 }
--- src/test/java/com.pelensky.httpserver/HttpServerTest.java
+++ src/test/java/com.pelensky.httpserver/HttpServerTest.java
@@ -3,14 +3,20 @@ package com.pelensky.httpserver;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class HttpServerTest {
 
     private HttpServer server;
-    private Integer port = 5000;
+    private Integer port = 1234;
     private String directory = "/Users/dan/Server/cob_spec/public/";
+    private String host = "localhost";
 
     @Before
     public void setUp() {
@@ -18,12 +24,19 @@ public class HttpServerTest {
     }
 
     @Test
-    public void httpServerHasHost() {
+    public void serverHasHost() {
         assertEquals(server.getDirectory(), directory);
     }
 
     @Test
-    public void httpServerHasPort() {
+    public void serverHasPort() {
         assertEquals(server.getPort(), port);
+    }
+
+    @Test
+    public void serverConnectsToClient() throws IOException {
+        server.serve();
+        Socket client = new Socket(host, port);
+        assertTrue(client.isConnected());
     }
 }
