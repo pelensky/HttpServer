4ef1d6a00ecd1a573b0af96c61fc7bc0c3549df8
Dan Pelensky <dan@pelensky.com>
2017-09-21 16:53:36 +0100

    FileProcessor and Range now use int[] for most methods

--- src/main/java/com/pelensky/httpserver/Routes/Default.java
+++ src/main/java/com/pelensky/httpserver/Routes/Default.java
@@ -4,6 +4,7 @@ import com.pelensky.httpserver.File.FileProcessor;
 import com.pelensky.httpserver.Request.Request;
 import com.pelensky.httpserver.Response.Response;
 import com.pelensky.httpserver.Response.Status;
+import com.pelensky.httpserver.Utilities.HtmlFormatter;
 
 import java.io.IOException;
 
@@ -16,7 +17,7 @@ public class Default extends Route {
 
     @Override
     public Response get(Request request) throws IOException {
-        return new Response(Status.OK.code(), null, new FileProcessor().displayDirectoryContentsAsLinks());
+        return new Response(Status.OK.code(), null, new HtmlFormatter().format("HttpServer", new FileProcessor().getPublicFolderContents()).getBytes());
     }
 
     @Override
