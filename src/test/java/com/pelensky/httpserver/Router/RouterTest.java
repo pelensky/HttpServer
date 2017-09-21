72faa5f17bf04e96a25150be876aa09033facc47
Dan Pelensky <dan@pelensky.com>
2017-09-21 17:13:11 +0100

    Puts test back in

--- src/test/java/com/pelensky/httpserver/Router/RouterTest.java
+++ src/test/java/com/pelensky/httpserver/Router/RouterTest.java
@@ -140,6 +140,14 @@ public class RouterTest {
     }
 
     @Test
+    public void LetsAuthorisedUserIn() throws IOException, NoSuchAlgorithmException {
+        Request request = setUpRequest("GET /logs HTTP/1.1\n");
+        assertEquals("HTTP/1.1 401\nWWW-Authenticate: Basic realm=\"Authentication required\"", getResponse(request));
+        Request request1 = setUpRequest("GET /logs HTTP/1.1\nAuthorization: Basic YWRtaW46aHVudGVyMg==\n");
+        assertTrue(getResponse(request1).contains("200"));
+    }
+
+    @Test
     public void decodesParameters() throws IOException, NoSuchAlgorithmException {
         Request request = setUpRequest("GET /parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff HTTP/1.1\n");
         assertEquals("HTTP/1.1 200\nContent-Length: 96\n\nvariable_1 = Operators <, >, =, !=; +, -, *, &, @, #, $, [, ]: \"is that all\"?\nvariable_2 = stuff", getResponse(request));
